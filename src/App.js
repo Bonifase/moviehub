c29bf07bb9d210198a093581c94271c6aaf3d693
Bonifase <orwabonifase@gmail.com>
2019-02-24 10:41:46 +0300

    fetch the first data

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import './App.css';
 import Home from './components/Home';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        data: [],
+        response: 'got reponse'
+    }
+}
   componentDidMount() {
-    Axios.get(`${process.env.REACT_APP_API_URL}movie/550?api_key=${process.env.REACT_APP_API_KEY}`)
+    Axios.get(`${process.env.REACT_APP_API_URL}movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}`)
     .then(res => {
-        console.log(res)
+        let movies = res.data.results;
+        this.setState({ data: movies })
+        // console.log(movies);
     })
 }
 
   render() {
+    let data = this.state.data
+    const movieItems = data.map((d) => <li key={d.id}>{d.title}</li>);
     return (
       <div className="App">
         <header className="App-header">
          <Home />
+         { movieItems }
         </header>
       </div>
     );
